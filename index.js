21165c5964234d80e4cab2816a89f767c7bb9e68
Isaac Munene Ndereba <abeyesark@gmail.com>
2023-09-27 22:07:53 +0300

    added fetch option, either works

--- index.js
+++ index.js
@@ -2,15 +2,51 @@ import get from "axios";
 import { load } from "cheerio";
 
 const url = "https://news.ycombinator.com/";
-get(url)
-  .then((response) => {
-    const $ = load(response.data);
+
+//get via Axios
+function getYCDataAxios() {
+  get(url)
+    .then((response) => {
+      const $ = load(response.data);
+      const titles = [];
+      $(".titleline > a").each((index, element) => {
+        titles.push($(element).text());
+      });
+
+      console.log("Axios Data begins here...");
+      console.log(
+        "=============================================================="
+      );
+      console.log(titles);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// get data via fetch
+async function getYCData() {
+  try {
+    // Fetch data from URL and store the response into a const
+    const response = await fetch(url);
+    // Convert the response into text
+    const body = await response.text();
+
+    // Load body data
+    const $ = load(body);
     const titles = [];
     $(".titleline > a").each((index, element) => {
       titles.push($(element).text());
     });
+    console.log("Fetch Data begins here...");
+    console.log(
+      "=============================================================="
+    );
     console.log(titles);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getYCData();
+getYCDataAxios();
